02e9cd780ff77e40b32e50c8e87e9db089c5e028
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2014-04-01 13:16:33 +0100

    tosulliv: fix to unit test to instantiate google analytics at correct point in test

--- SupportingLifeAndroidAppParent/SupportingLifeAndroidProject-IT/src/main/java/ie/ucc/bis/supportinglife/activity/test/HomeActivityTest.java
+++ SupportingLifeAndroidAppParent/SupportingLifeAndroidProject-IT/src/main/java/ie/ucc/bis/supportinglife/activity/test/HomeActivityTest.java
@@ -8,14 +8,22 @@ import com.google.analytics.tracking.android.GoogleAnalytics;
 public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
 
     public HomeActivityTest() {
-        super(HomeActivity.class);
-        
+        super(HomeActivity.class);        
+    }
+    
+	@Override
+	public void setUp() {	
+        // turn off google analytics for duration of test
+        turnOffGoogleAnalytics();
+	}
+
+    private void turnOffGoogleAnalytics() {
         // turn off google analytics for duration of test
         GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(getActivity());
         googleAnalytics.setAppOptOut(true);
-    }
+	}
 
-    public void testActivity() {
+	public void testActivity() {
     	assertNotNull("activity should be launched successfully", getActivity());
     }
 }
